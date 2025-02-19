8e3fdc0d94d5a1b100499605933304ed0e73d228
Lawrence D. Kern <law@lawkern.com>
2025-02-18 21:24:07 -0500

    Read protocol from incoming URL prefix

--- src/main.adb
+++ src/main.adb
@@ -5,6 +5,28 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Main is
+   Scheme : constant String := "gemini";
+   Host   : constant String := "geminiprotocol.net";
+   Port   : constant String := "1965";
+   Path   : constant String := "/";
+
+   Url : constant String := Scheme & "://" & Host & Path;
+
+   function Has_Prefix (S, Prefix : String) return Boolean is
+   begin
+      if S'Length < Prefix'Length then
+         return False;
+      end if;
+
+      return Prefix = S (S'First .. S'First + Prefix'Length - 1);
+   end Has_Prefix;
+
 begin
    Put_Line ("BROWSER V0.0");
+
+   if Has_Prefix(Url, "gemini://") then
+      Put_line (Url);
+   else
+      Put_line ("ERROR: Unsupported protocol.");
+   end if;
 end Main;
